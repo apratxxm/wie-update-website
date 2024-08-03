6b7f45d029450c80329fa15ea6bfdf340d21513d
Apratim Pandey <apratxm@gmail.com>
2024-08-04 05:02:34 +0530

    initial commit

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>hello WIE</h1>
+    <main className="flex justify-center items-center h-dvh">
+      <h1 className="text-[8rem] inline-block">Hello WIE</h1>
+    </main>
   );
 }
